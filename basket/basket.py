fc8951f9a1406f2810047a83f3015fb5a70779f0
Francisco Sabino <fdasabino@gmail.com>
2021-09-22 01:39:34 +0200

    add cart function that allows users to add items

--- basket/basket.py
+++ basket/basket.py
@@ -13,7 +13,10 @@ class Basket():
         product_id = product.id
 
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(
-                product.price), 'qty': int(qty)}
+            self.basket[product_id] = {
+                'price': str(product.price), 'qty': int(qty)}
 
         self.session.modified = True
+
+    def __len__(self):
+        return sum(item['qty']for item in self.basket.values())
