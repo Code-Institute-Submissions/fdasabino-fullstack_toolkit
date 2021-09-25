3b0b488b812269d9afb34b3e9dcaf2a47d3b006f
Francisco Sabino <fdasabino@gmail.com>
2021-09-25 16:48:06 +0200

    adjusted order model

--- order/models.py
+++ order/models.py
@@ -6,8 +6,6 @@ from store.models import Product
 
 
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE, related_name='order_user')
     full_name = models.CharField(max_length=50)
     address1 = models.CharField(max_length=250)
     address2 = models.CharField(max_length=250)
