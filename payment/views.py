4d492f2f785a92f90c16f589a5777932e7c3d801
Francisco Sabino <fdasabino@gmail.com>
2021-09-25 18:06:07 +0200

    add payment page template and install stripe package

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,21 @@
+import stripe
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from basket.basket import Basket
 
-# Create your views here.
+@login_required
+def BasketView(request):
+    basket = Basket(request)
+    total = str(basket.get_total_price())
+    total = total.replace('.', '')
+    total = int(total)
+    
+    stripe.api_key = 'pk_test_51JdcCvLxCavNaE0z8e1Ef5xTfVgdLoa87K7EjtWMhu66bH0fncbVYJPji3E4M3CUPq9rQuU0g4z8jDiRpdS4p1Oy003t5zqzFT'
+    intent = stripe.PaymentIntent.create(
+        amount=total,
+        currency='SEK',
+        metadata={'user_id': request.user.id}
+    )
+    
+    return render(request, 'payment/payment_form.html', {'client_secret': intent.client_secret})
+        
