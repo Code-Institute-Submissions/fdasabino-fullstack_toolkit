7098373f544c78d40f268cbce909de49672f8e42
Francisco Sabino <fdasabino@gmail.com>
2021-09-28 21:32:11 +0200

    optimize data model and store models and views

--- store/views.py
+++ store/views.py
@@ -12,16 +12,16 @@ def about(request):
 
 
 def product_all(request):
-    products = Product.products.all()
+    products = Product.objects.all()
     return render(request, "store/order.html", {"products": products})
 
 
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
-    products = Product.products.filter(category=category)
+    products = Product.objects.filter(category=category)
     return render(request, "store/category.html", {"category": category, "products": products})
 
 
 def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    product = get_object_or_404(Product, slug=slug, is_active=True)
     return render(request, "store/product_detail.html", {"product": product})
