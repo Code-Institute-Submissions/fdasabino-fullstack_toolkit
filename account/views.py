966eb5f0003d12afd732d238384d563c72bc330e
Francisco Sabino <fdasabino@gmail.com>
2021-09-29 11:14:12 +0200

    added wishlist/favorites section to user dashboard

--- account/views.py
+++ account/views.py
@@ -1,17 +1,37 @@
+from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from order.views import user_orders
+from store.models import Product
 
 from .forms import RegistrationForm, UserAddressForm, UserEditForm
 from .models import Address, Customer
 from .tokens import account_activation_token
+
+
+@login_required
+def wishlist(request):
+    products = Product.objects.filter(users_wishlist=request.user)
+    return render(request, "account/dashboard/wishlist.html", {"wishlist": products})
+
+
+@login_required
+def add_to_wishlist(request, id):
+    product = get_object_or_404(Product, id=id)
+    if product.users_wishlist.filter(id=request.user.id).exists():
+        product.users_wishlist.remove(request.user)
+        messages.success(request, product.title + " Was removed from Your Favorites")
+    else:
+        product.users_wishlist.add(request.user)
+        messages.success(request, product.title + " Was Added to Your Favorites")
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
 
 
 @login_required
